3da631313a21e871a140986616c7b41a8b9af995
Dmytro Mysak <dmytro.mysak@cyberproof.com>
2023-04-11 17:08:58 +0200

    Fixed cors

--- netlify/functions/data.js
+++ netlify/functions/data.js
@@ -3,7 +3,12 @@ exports.handler = function (event, context) {
 
   return fetch(event.queryStringParameters.url).then((res) =>
     res.text().then((body) => ({
-      headers: { "Content-type": "application/json" },
+      headers: {
+        "Access-Control-Allow-Origin": "*",
+        "Access-Control-Allow-Headers": "Content-Type",
+        "Access-Control-Allow-Methods": "GET, POST, PUT, DELETE",
+        "Content-type": "application/json",
+      },
       statusCode: 200,
       body: body,
     }))
